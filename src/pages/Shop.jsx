3331277d3a0ca7d190610a567801ec7aa8fd78a3
PrapooRozario <albart2022@gmail.com>
2025-01-17 11:49:32 +0600

    Added Sorting and Search functionality

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -10,37 +10,90 @@ import {
 } from "@/components/ui/table";
 import useAxios from "@/hooks/useAxios";
 import { useQuery } from "@tanstack/react-query";
-import { Eye } from "lucide-react";
+import { Eye, Search } from "lucide-react";
 import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Input } from "@headlessui/react";
 
 const Shop = () => {
   const [axiosSecure] = useAxios(); // Custom hook for secure axios instance
   const [discount, setDiscount] = useState(0); // State to track discount
-  const [currentPage, setCurrentPage] = useState(2);
-  const { data: products = [] } = useQuery({
-    queryKey: ["products", currentPage],
+  const [sort, setSort] = useState("");
+  const [search, setSearch] = useState("");
+  const [totalProducts, setTotalProducts] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+  useEffect(() => {
+    if (totalProducts <= 10) {
+      setCurrentPage(0);
+    }
+  }, [totalProducts]);
+  const {
+    data: products = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["products", currentPage, sort, search],
     queryFn: async () => {
       const res = await axiosSecure.get(
-        `/products?limit=${itemsPerPage}&skip=${currentPage * itemsPerPage}`
+        `/products?limit=${itemsPerPage}&skip=${
+          currentPage * itemsPerPage
+        }&sort=${sort}&search=${search}`
       );
-      return res.data; // Return fetched product data
-    },
-  });
-  const { data: totalProducts = [] } = useQuery({
-    queryKey: ["totalProducts"],
-    queryFn: async () => {
-      const res = await axiosSecure.get(`/products/total`);
-      return res.data; // Return fetched product data
+      setTotalProducts(res.data?.productsCount);
+      return res.data.products; // Return fetched product data
     },
   });
   const itemsPerPage = 10;
-  const totalItems = totalProducts?.total || 0;
-  const totalPages = totalItems ? Math.ceil(totalItems / itemsPerPage) : 0;
-  console.log(currentPage, totalPages, totalItems);
+  const totalPages = totalProducts
+    ? Math.ceil(totalProducts / itemsPerPage)
+    : 0;
   return (
     <div className="my-10 px-4 md:px-6 lg:px-8">
-      <Table className="w-full overflow-x-auto">
+      {!products.length && !isLoading ? (
+        <div className="absolute top-[56%] z-10 right-[20%] left-[20%] flex justify-center items-center">
+          <h1 className="md:text-2xl text-center text-lg font-medium">
+            Oops! We couldn’t find what you’re looking for.
+          </h1>
+        </div>
+      ) : (
+        ""
+      )}
+      {isError && (
+        <div className="absolute top-[56%] z-10 right-[20%] left-[20%] flex justify-center items-center">
+          <h1 className="md:text-2xl text-center text-lg font-medium">
+            Oops! Something went wrong.
+          </h1>
+        </div>
+      )}
+      {isLoading && (
+        <div className="h-[calc(100vh-100px)] absolute right-[20%] left-[20%] flex justify-center items-center">
+          <div className="spinner"></div>
+        </div>
+      )}
+      <div className="mb-6 md:flex items-center justify-between">
+        <Button
+          onClick={() => {
+            if (sort === "") {
+              setSort("desc");
+            } else if (sort === "desc") {
+              setSort("asc");
+            } else {
+              setSort("");
+            }
+          }}
+          className={buttonVariants({ variant: "primary" })}
+        >
+          {sort === "desc" || sort === "asc" ? "Sorted" : "Sort"} By Price
+        </Button>
+        <div className="relative flex items-center md:mt-0 mt-4">
+          <Input
+            onChange={(e) => setSearch(e.target.value)}
+            className="border rounded-lg pr-4 pl-9 py-2 outline-none md:w-[360px] w-full"
+          ></Input>
+          <Search className="absolute top-[9px] w-5 mx-2 text-neutral-600"></Search>
+        </div>
+      </div>
+      <Table className="w-full overflow-x-auto min-h-[calc(100vh-300px)]">
         <TableHeader>
           <TableRow>
             <TableHead>#</TableHead>
@@ -54,7 +107,7 @@ const Shop = () => {
         <TableBody>
           {products?.map((product, index) => (
             <TableRow key={product?._id}>
-              <TableCell>{index + 1}</TableCell>
+              <TableCell>{currentPage * itemsPerPage + index + 1}</TableCell>
               <TableCell className="font-medium">
                 <div className="bg-blue-100 rounded-xl p-2">
                   <img
@@ -148,7 +201,7 @@ const Shop = () => {
           ))}
         </TableBody>
       </Table>
-      <div className="flex items-center gap-2 text-sm justify-center mt-6">
+      <div className="flex flex-wrap items-center gap-2 text-sm justify-center mt-6">
         {/* Previous Button */}
         <button
           onClick={() => currentPage > 0 && setCurrentPage(currentPage - 1)}
@@ -159,7 +212,7 @@ const Shop = () => {
         </button>
 
         {/* Page Number Buttons */}
-        {Array.from({ length: totalPages }).map((_, index) => (
+        {Array.from({ length: totalPages || 1 }).map((_, index) => (
           <div
             key={index}
             className={`bg-[#1158dbe7] px-4 py-2 text-white rounded-xl font-medium cursor-pointer ${
