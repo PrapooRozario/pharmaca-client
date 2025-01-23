ab794772daa16afec3eec62eff2d298fe0fcb710
PrapooRozario <albart2022@gmail.com>
2025-01-23 23:11:22 +0600

    Banner responsive fixed

--- src/shared/Slide.jsx
+++ src/shared/Slide.jsx
@@ -4,7 +4,7 @@ const Slide = ({ image, title, description, color }) => {
   return (
     <div
       style={{ backgroundColor: color }}
-      className="flex flex-col-reverse md:flex-row gap-6 sm:flex-row justify-center items-center md:px-16 px-6 md:py-0 py-6 rounded-xl"
+      className="flex flex-col-reverse h-[520px] md:h-[420px] md:flex-row gap-6 sm:flex-row justify-center items-center md:px-16 px-6 md:py-0 py-6 rounded-xl"
     >
       <div className="text-center md:text-left sm:text-left">
         <h1 className="lg:text-5xl md:text-4xl sm:text-4xl text-2xl text-white font-medium">
@@ -12,7 +12,7 @@ const Slide = ({ image, title, description, color }) => {
         </h1>
         <p className="text-white/80 md:text-lg md:mt-4 mt-2">{description}</p>
       </div>
-      <img src={image} alt={title} className="md:w-2/4 sm:w-2/4 w-full" />
+      <img src={image} alt={title} className="md:w-2/4 md:h-[400px] sm:w-2/4" />
     </div>
   );
 };
